3a963283332864cfd9d7855643599598a8a5fe43
Rachel BANDIERA <rachel.bandiera@gmail.com>
2020-08-17 13:47:41 +0200

    TP02

--- Test-Jpa/src/test/java/jpa01/TestJpa.java
+++ Test-Jpa/src/test/java/jpa01/TestJpa.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 import org.junit.Test;
 import entity.livre;
@@ -14,8 +15,8 @@ public class TestJpa
 	private EntityManagerFactory factory = null;
 
 	@Test
-	public void test() {
-		factory = Persistence.createEntityManagerFactory("pu_essai");
+	public void findLivre() {
+		//factory = Persistence.createEntityManagerFactory("pu_essai");
 		EntityManager em = factory.createEntityManager();
 		
 		if(em != null)
@@ -24,6 +25,29 @@ public class TestJpa
 			System.out.println(livre1);
 			
 		}
+		
+		em.close();
+		factory.close();
+		
+	}
+	
+	public void listeLivres()
+	{
+		EntityManager em = factory.createEntityManager();
+		
+		if(em != null)
+		{
+			String query = "SELECT 1 FROM Livre 1";
+			TypedQuery<livre> q = em.createQuery(query, livre.class);
+			for(livre l : q.getResultList())
+			{
+				System.out.println(l);
+			}
+			
+		}
+		
+		em.close();
+		factory.close();
 		
 	}
 
